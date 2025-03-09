5529cf884b4e30cc29ef712d03e73e3af9f8961c
bryanmalpartida <bmalpartidatrelles@byupathway.edu>
2025-03-08 22:38:11 -0500

    u

---
+++ scripts/chamber.js
@@ -0,0 +1,33 @@
+//HAMBURGUER BAR ON NAV BAR
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navigation");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+
+
+
+
+
+//LAST MODIFIED FOOTER
+
+// giving variable to last modified
+const lastModifiedDate = document.lastModified;
+
+// using another variable so we can call lastmodified by id
+const lastModifiedParagraph = document.getElementById('lastModified');
+
+// desired text+outcome
+lastModifiedParagraph.textContent = `Last Modification: ${lastModifiedDate}`;
+
+//YEAR  ON FOOTER
+
+//giving variable to year
+const year = document.querySelector("#year");
+//variable for current year 
+const today = new Date();
+//print
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
