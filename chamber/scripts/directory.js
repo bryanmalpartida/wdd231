43c55d34fc931fbf12ef7e344dddbd3cd04df22f
bryanmalpartida <bmalpartidatrelles@byupathway.edu>
2025-03-15 10:40:23 -0500

    u

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,41 @@
+//HAMBURGUER BAR ON NAV BAR
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navigation");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+
+//LAST MODIFIED FOOTER
+
+// giving variable to last modified
+const lastModifiedDate = document.lastModified;
+
+// using another variable so we can call lastmodified by id
+const lastModifiedParagraph = document.getElementById('lastModified');
+
+// desired text+outcome
+lastModifiedParagraph.textContent = `Last Modification: ${lastModifiedDate}`;
+
+//YEAR  ON FOOTER
+
+//giving variable to year
+const year = document.querySelector("#year");
+//variable for current year 
+const today = new Date();
+//print
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+
+//json//
+const url= 'https://bryanmalpartida.github.io/wdd231/chamber/members.json';
+const cards = document.querySelector('#cards');
+
+async function getMembersData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayMembers(data.members);
+  }
+
